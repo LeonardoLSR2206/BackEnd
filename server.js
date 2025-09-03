f5d4a28841e95dd001b0fe64b80eb9d0fa51b886
LeonardoLSR2206 <leonardolsr2206@gmail.com>
2025-09-03 11:41:46 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+// Importar bibliotecas
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+// Configurar servidor
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+
+// Criar banco sqlite3
+const db = new sqlite3.Database("./database.db");
+
+// Criar tabela usuários
+db.run(`CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    email TEXT,
+    senha TEXT
+    )
+`);
+
+// Cadastrar usuário
+app.post("/usuarios", async (req, res) =>{
+    let nome = req.body.nome;
+    let email = req.body.email;
+    let senha = req.body.senha;
+
+    let senhaHash = await bcrypt.hash(senha, 10);
+
+    // Inserir no banco de dados
+    db.run(`INSERT usuarios (nome, email, senha)
+        VALUES (?, ?, ?)`,
+        [nome, email, senhaHash],
+        res.json({
+            id: this.lastID,
+            nome,
+            email
+        })
+    )
+});
