62c15f27269bd255884a9db44151a8bd1ed1c860
LeonardoLSR2206 <leonardolsr2206@gmail.com>
2025-09-15 11:43:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -41,3 +41,13 @@ app.post("/usuarios", async (req, res) =>{
         })
     )
 });
+
+// Listar todos os usuários
+app.get("/usuarios", (req, res) => {
+    db.all(`SELECT id, nome, email FROM usuarios`, [], (err, rows) => {
+        res.json(rows)
+    })
+})
+
+// Iniciar o server
+app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`))
